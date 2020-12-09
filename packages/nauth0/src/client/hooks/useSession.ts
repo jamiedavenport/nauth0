3c938f3cb3bd40e7aa4ead1f55aa991c71a64e70
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-09 18:37:49 +0000

    Update useSession type

--- packages/nauth0/src/client/hooks/useSession.ts
+++ packages/nauth0/src/client/hooks/useSession.ts
@@ -1,7 +1,7 @@
 import { Session } from 'src/lib';
 
 // TODO: Implement this hook
-export const useSession = async (): Promise<Session> => {
+export const useSession = (): Session => {
   return {
     loading: true,
   };
