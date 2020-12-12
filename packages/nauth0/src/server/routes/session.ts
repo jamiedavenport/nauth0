8f06d623b018e073e4e9a8a117a1e25b1310a4d9
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-12 17:55:58 +0000

    feat: Implement session route (#21)

--- packages/nauth0/src/server/routes/session.ts
+++ packages/nauth0/src/server/routes/session.ts
@@ -1,8 +1,23 @@
+import { verify } from 'jsonwebtoken';
+import { parseCookies } from 'nookies';
+import { sessionCookie } from '../cookies';
+import { isValidToken } from '../session';
 import { NAuth0ApiRoute } from './route';
 
-export const sessionRoute: NAuth0ApiRoute = (req, res) => {
-  // TODO: Implement the session route
-  // 1. Read session from the cookie (session store)
+export const sessionRoute: NAuth0ApiRoute = (req, res, opts) => {
+  const cookies = parseCookies({ req });
+  const rawToken = cookies[sessionCookie];
 
-  res.end('session');
+  if (typeof rawToken === 'undefined') {
+    res.status(401).end();
+    return;
+  }
+
+  const token = verify(rawToken, opts.session.cookieSecret);
+
+  if (!isValidToken(token)) {
+    throw new Error('Decoded token was not of type `object`');
+  }
+
+  res.json(token.session);
 };
--- packages/nauth0/src/server/session.ts
+++ packages/nauth0/src/server/session.ts
@@ -3,6 +3,10 @@ import { Session } from 'src/lib';
 import { sign } from 'jsonwebtoken';
 import { NAuth0Config } from './config';
 
+export interface Token {
+  session: Session;
+}
+
 export const sessionFromTokenSet = (tokenSet: TokenSet): Session => {
   const claims = tokenSet.claims();
   return {
@@ -14,5 +18,20 @@ export const sessionFromTokenSet = (tokenSet: TokenSet): Session => {
 };
 
 export const encodeSession = (session: Session, opts: NAuth0Config): string => {
-  return sign(session, opts.session.cookieSecret);
+  return sign({ session }, opts.session.cookieSecret);
+};
+
+export const isValidToken = (
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  decodedToken: string | object
+): decodedToken is Token => {
+  if (typeof decodedToken !== 'object') {
+    return false;
+  }
+
+  if ((decodedToken as Token).session === undefined) {
+    return false;
+  }
+
+  return true;
 };
