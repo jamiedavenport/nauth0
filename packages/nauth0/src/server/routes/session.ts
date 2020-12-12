6b293a7e0c2935cd3b231bbfbcb4e247326a1a4e
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-12 16:54:56 +0000

    Return just the session and not extra token data

--- packages/nauth0/src/server/routes/session.ts
+++ packages/nauth0/src/server/routes/session.ts
@@ -1,18 +1,23 @@
 import { verify } from 'jsonwebtoken';
 import { parseCookies } from 'nookies';
 import { sessionCookie } from '../cookies';
+import { isValidToken } from '../session';
 import { NAuth0ApiRoute } from './route';
 
 export const sessionRoute: NAuth0ApiRoute = (req, res, opts) => {
   const cookies = parseCookies({ req });
-  const rawSession = cookies[sessionCookie];
+  const rawToken = cookies[sessionCookie];
 
-  if (typeof rawSession === 'undefined') {
+  if (typeof rawToken === 'undefined') {
     res.status(401).end();
     return;
   }
 
-  const session = verify(rawSession, opts.session.cookieSecret);
+  const token = verify(rawToken, opts.session.cookieSecret);
 
-  res.json(session);
+  if (!isValidToken(token)) {
+    throw new Error('Decoded token was not of type `object`');
+  }
+
+  res.json(token.session);
 };
