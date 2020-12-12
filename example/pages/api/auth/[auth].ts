2a394a8da4e8b1fbc1c3aca285d0c4b97686e7ea
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-12 20:04:31 +0000

    Move server code to @nauth0/server package

--- example/pages/api/auth/[auth].ts
+++ example/pages/api/auth/[auth].ts
@@ -1,6 +1,6 @@
-import { apiHandler } from 'nauth0/dist/server';
+import nauth0 from '@nauth0/server';
 
-export default apiHandler({
+export default nauth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
