9233ad7daaf6a99372811423335c53c56c103cab
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-18 11:25:51 +0000

    Implement logout route

--- packages/nauth0/src/server/routes/logout.ts
+++ packages/nauth0/src/server/routes/logout.ts
@@ -1,10 +1,21 @@
+import { destroyCookie } from 'nookies';
+import { sessionCookie, stateCookie } from '../cookies';
 import { NAuth0ApiRoute } from './route';
 
-export const logoutRoute: NAuth0ApiRoute = (req, res) => {
-  // TODO: Implement the logout route
-  // https://auth0.com/docs/flows/authorization-code-flow
-  // 1. Redirect to Auth0 to logout
-  // 2. Void the session cookies
+export const logoutRoute: NAuth0ApiRoute = async (req, res, opts) => {
+  // TODO: This is very auth0 specific. If we want to make this work with any OIDC provider then it needs to change to use the end_session_url from .well-known
+  const endSessionUrl = `https://${opts.domain}/v2/logout?client_id=${
+    opts.clientId
+  }&returnTo=${encodeURIComponent(opts.logoutRedirectUri)}`;
 
-  res.end('Logout');
+  destroyCookie({ res }, stateCookie);
+  destroyCookie({ res }, sessionCookie, {
+    path: '/',
+  });
+
+  res
+    .writeHead(302, {
+      Location: endSessionUrl,
+    })
+    .end();
 };
