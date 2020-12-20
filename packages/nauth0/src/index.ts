f152e2cc15883d7d8f0317220e2509975a17f7ad
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-12-20 14:59:23 +0000

    Add the oidc client refreshFunction

--- packages/nauth0/src/index.ts
+++ packages/nauth0/src/index.ts
@@ -1,9 +1,12 @@
 import BrowserNAuth0Client from './browser';
 import { NAuth0Client } from './client';
+import { Session } from './lib';
 import ServerNAuth0Client, { NAuth0Options } from './server';
 import OidcClientProvider from './server/OidcClientProvider';
 import { RouteHandler } from './server/RouteHandler';
+import { sessionFromTokenSet } from './server/session';
 import { CookieSessionStore } from './server/session/CookieSessionStore';
+import RefreshingSessionStore from './server/session/RefreshingSessionStore';
 
 export default (opts: NAuth0Options): NAuth0Client => {
   const isBrowser = typeof window !== 'undefined';
@@ -13,7 +16,20 @@ export default (opts: NAuth0Options): NAuth0Client => {
   }
 
   const oidcClientProvider = new OidcClientProvider(opts);
-  const sessionStore = new CookieSessionStore(opts);
+  const refreshFunction = async ({
+    refreshToken,
+  }: Session): Promise<Session> => {
+    if (typeof refreshToken === 'undefined') {
+      throw new Error('Missing refresh token'); // TODO: Improve this
+    }
+    const client = await oidcClientProvider.getClient();
+    const tokenSet = await client.refresh(refreshToken);
+    return sessionFromTokenSet(tokenSet);
+  };
+  const sessionStore = new RefreshingSessionStore(
+    new CookieSessionStore(opts),
+    refreshFunction
+  );
   const routeHandler = new RouteHandler(opts, sessionStore, oidcClientProvider);
   return new ServerNAuth0Client(opts, routeHandler, sessionStore);
 };
