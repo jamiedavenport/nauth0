d3d25065ccbcd00e6184f2711fd11a83c87a02be
Mark Laing <m.p.laing@outlook.com>
2020-12-21 09:21:45 +0000

    Adds redirectToFromReq function to extract the query parameter.

--- packages/nauth0/src/server/routes/login.ts
+++ packages/nauth0/src/server/routes/login.ts
@@ -3,16 +3,23 @@ import { createClient, createState } from '../oidc';
 import { setCookie } from 'nookies';
 import { stateCookie } from '../cookies';
 import { redirectKey } from '../config';
+import { NextApiRequest } from 'next';
+
+const redirectToFromReq = (req: NextApiRequest): string | null => {
+  const param = req.query[redirectKey];
+  if (typeof param === 'string') {
+    return param;
+  } else if (Array.isArray(param) && typeof param[0] === 'string') {
+    return param[0];
+  }
+  return null;
+};
 
 export const loginRoute: NAuth0ApiRoute = async (req, res, opts) => {
   const client = await createClient(opts);
-  const state = createState(
-    req.query[redirectKey]
-      ? {
-          [redirectKey]: req.query[redirectKey],
-        }
-      : {}
-  );
+  const state = createState({
+    [redirectKey]: redirectToFromReq(req),
+  });
 
   const authorizationUrl = client.authorizationUrl({
     redirect_uri: opts.redirectUri,
