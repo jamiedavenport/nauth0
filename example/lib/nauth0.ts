554a7047cf5617d1e275cf5a9e216a0c02349ebb
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-01-05 15:31:24 +0000

    Rename env variables

--- example/lib/nauth0.ts
+++ example/lib/nauth0.ts
@@ -1,9 +1,9 @@
 import nauth0 from 'nauth0';
 
 export default nauth0({
-  domain: process.env.AUTH0_DOMAIN,
-  clientId: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  domain: process.env.AUTH_ISSUER,
+  clientId: process.env.AUTH_CLIENT_ID,
+  clientSecret: process.env.AUTH_CLIENT_SECRET,
   redirectUri: 'http://localhost:3000/api/auth/callback',
   logoutRedirectUri: 'http://localhost:3000/',
   scope: 'openid profile offline_access email',
